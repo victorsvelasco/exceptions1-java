1892690e4c44c237c934a57dd66f6b43f394dfc3
victorsvelasco <victorsvelasco@gmail.com>
2022-02-11 11:13:02 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -13,6 +15,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -42,10 +47,10 @@ public class Reservation {
 	public String updateDates(Date checkIn, Date checkOut) {
 		Date date = new Date();
 		if (checkIn.before(date) || checkOut.before(date)) {
-			return "Reservation dates for update must be future dates";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Reservation dates for update must be future dates";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -62,7 +67,7 @@ public class Reservation {
 				+ sdf.format(checkOut)
 				+ ", "
 				+ duration()
-				+ "nights";
+				+ " nights";
 	
 	
 	}
